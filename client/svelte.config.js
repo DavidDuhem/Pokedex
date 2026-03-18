f22f113f85f825841d30c3500180061ce3009d0c
David D <synlike@vps-b17a40d4.vps.ovh.net>
2026-03-18 15:44:45 +0000

    config prod env

--- client/svelte.config.js
+++ client/svelte.config.js
@@ -6,6 +6,9 @@ const config = {
 	preprocess: vitePreprocess(),
 	kit: {
 		adapter: adapter(),
+		paths: {
+            		base: process.env.NODE_ENV === 'production' ? '/pokedex' : '',
+        	},
 		alias: {
 			$components: 'src/lib/components',
 			'@pokedex/shared': '../shared',
